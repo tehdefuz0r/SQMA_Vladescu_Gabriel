d1f53529778525534e887ca991d032a8d29f6ea5
Vladescu Gabriel <40556844+tehdefuz0r@users.noreply.github.com>
2020-12-10 21:27:44 +0200

    Add files via upload

---
+++ Generic.java
@@ -0,0 +1,12 @@
+
+public class Generic {
+	
+	public static int multiplyAndSquare(int param1, int param2) {
+		return (int) Math.pow((param1 * param2),2);
+	}
+	
+	public static String concatAndUpper(String one, String two) {
+		return one.toUpperCase() + two.toUpperCase();
+	}
+	
+}
